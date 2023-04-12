bf9b72cf0f2f6a5b7b6ca87d513b0a0145e7ed11
chaeryeong1 <94097668+chaeryeong1@users.noreply.github.com>
2023-04-12 17:52:47 +0900

    Delete d directory

--- d/renderer.js
+++
@@ -1,7 +0,0 @@
-const remote = require('electron').remote;
-const closeButton = document.getElementById('closeButton');
-
-closeButton.addEventListener('click', function(){
-    var window = remote.getCurrentWindow();
-    window.close();
-})
