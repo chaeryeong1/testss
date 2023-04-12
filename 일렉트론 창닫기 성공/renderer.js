3a89b19c703bd764d44e3f0759456605ca64c71b
chaeryeong1 <94097668+chaeryeong1@users.noreply.github.com>
2023-04-12 20:27:00 +0900

    Delete 일렉트론 창닫기 성공 directory

--- 일렉트론 창닫기 성공/renderer.js
+++
@@ -1,6 +0,0 @@
-const { ipcRenderer } = require('electron');
-
-const closeButton = document.getElementById('closeButton');
-closeButton.addEventListener('click', function () {
-    ipcRenderer.send('close-window');
-});
