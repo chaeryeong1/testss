1ea2ee38aded83f54beba02224ee36c497dad64c
chaeryeong1 <94097668+chaeryeong1@users.noreply.github.com>
2023-04-12 17:23:51 +0900

    Delete 일렉트론 창닫기 테스트 directory

--- 일렉트론 창닫기 테스트/renderer.js
+++
@@ -1,7 +0,0 @@
-const { remote } = require('electron');
-const currentWindow = remote.getCurrentWindow();
-
-const closeButton = document.getElementById('closeButton');
-closeButton.addEventListener('click', function() {
-  currentWindow.close();
-});
